6915154a7c7e5cc43f2e26a711f883612ef09efe
YaelChar417 <yaelcharles@gmail.com>
2025-11-27 10:36:22 -0600

    Feature: shared preferences actualizado y repository impl para sudoku hecho utilizando su interfaz

---
+++ app/src/main/java/com/app/pokedexapp/data/local/preferences/SessionPreferences.kt
@@ -0,0 +1,50 @@
+package com.app.pokedexapp.data.local.preferences
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import com.app.pokedexapp.domain.model.SudokuGame
+import com.google.gson.reflect.TypeToken
+
+class SessionPreferences @Inject constructor(
+    @ApplicationContext context: Context,
+    private val gson: Gson
+){
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences(
+            PreferencesConstants.PREF_NAME,
+            Context.MODE_PRIVATE,
+        )
+
+    fun saveGame(game: SudokuGame) {
+        prefs
+            .edit()
+            .putString(PreferencesConstants.KEY_SAVED_GAME, gson.toJson(game))
+            .putLong(PreferencesConstants.KEY_LAST_PLAYED, System.currentTimeMillis())
+            .apply()
+    }
+
+    fun getSavedGame(): SudokuGame? {
+        val json = prefs.getString(PreferencesConstants.KEY_SAVED_GAME, null)
+
+        return try {
+            val type = object : TypeToken<SudokuGame>() {}.type
+            gson.fromJson(json, type)
+        } catch (e: Exception){
+            null
+        }
+    }
+
+    fun hasSavedGame(): Boolean {
+        return prefs.contains(PreferencesConstants.KEY_SAVED_GAME)
+    }
+
+    fun clearCache(){
+        prefs
+            .edit()
+            .clear()
+            .apply()
+    }
+}
---
+++ app/src/main/java/com/app/pokedexapp/data/repository/SudokuRepositoryImpl.kt
@@ -0,0 +1,47 @@
+package com.app.pokedexapp.data.repository
+
+import com.app.pokedexapp.data.local.preferences.SessionPreferences
+import com.app.pokedexapp.data.mapper.toDomain
+import com.app.pokedexapp.data.remote.api.SudokuApi
+import com.app.pokedexapp.domain.model.SudokuGame
+import com.app.pokedexapp.domain.repository.SudokuRepository
+import javax.inject.Inject
+
+class SudokuRepositoryImpl @Inject constructor(
+    private val api: SudokuApi,
+    private val preferences: SessionPreferences
+): SudokuRepository {
+    override suspend fun getNewGame(
+        size: Int,
+        difficulty: String
+    ): Result<SudokuGame> {
+        return try {
+            val response = api.getSudoku(
+                width = size,
+                height = size,
+                difficulty = difficulty,
+            )
+
+            val game = response.toDomain(
+                difficulty = difficulty,
+                size = size
+            )
+
+            Result.success(game)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun saveGame(game: SudokuGame) {
+        preferences.saveGame(game)
+    }
+
+    override suspend fun loadGame(): SudokuGame? {
+        return preferences.getSavedGame()
+    }
+
+    override suspend fun hasSavedGame(): Boolean {
+        return preferences.hasSavedGame()
+    }
+}
