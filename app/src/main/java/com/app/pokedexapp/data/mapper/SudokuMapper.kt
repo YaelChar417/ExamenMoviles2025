63ad95d6e2a9095382236ab9be7994951b8f11c1
YaelChar417 <yaelcharles@gmail.com>
2025-11-27 10:05:28 -0600

    Feature: mapper para pasar de celulas y tablero dto a un objeto de domain, funcion sugerida por gemini y ajustada manualmente

---
+++ app/src/main/java/com/app/pokedexapp/data/mapper/SudokuMapper.kt
@@ -0,0 +1,41 @@
+package com.app.pokedexapp.data.mapper
+
+import com.app.pokedexapp.data.remote.dto.SudokuDto
+import com.app.pokedexapp.domain.model.SudokuCell
+import com.app.pokedexapp.domain.model.SudokuGame
+
+fun SudokuDto.toDomain(
+    difficulty: String,
+    size: Int,
+): SudokuGame {
+    val cells = mutableListOf<List<SudokuCell>>()
+
+    for (row in 0 until size) {
+        val currentCells = mutableListOf<SudokuCell>()
+
+        for (column in 0 until size) {
+            val puzzleVal = this.puzzle[row][column]
+            val solutionVal = this.solution[row][column]
+            val isFromApi = (puzzleVal != null)
+
+            val cell = SudokuCell(
+                row = row,
+                column = column,
+                currentVal = puzzleVal,
+                correctVal = solutionVal,
+                isFromApi = isFromApi,
+                isError = false
+            )
+
+            currentCells.add(cell)
+        }
+        cells.add(currentCells)
+    }
+
+    return SudokuGame(
+        board = cells,
+        difficulty = difficulty,
+        size = size,
+        isSolved = false,
+    )
+}
